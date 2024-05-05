acfcdc78a87c39c069c1d5616ed72ec17541d4b3
Trang Nguyen <nguyen7@kenyon.edu>
2024-05-05 19:25:13 +0000

    add files

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,55 +1,55 @@
 class JobPostsController < ApplicationController
-	before_action :find_job, only: [:show, :edit, :update, :destroy]
+  before_action :find_job_post, only: [:show, :edit, :update, :destroy]
 
-	def index
-		if params[:category].blank?
-			@jobs = Job.all.order("created_at DESC")
-		else
-			@category_id = Category.find_by(name: params[:category]).id
-			@jobs = Job.where(category_id: @category_id).order("created_at DESC")
-		end
-	end
+  def index
+    if params[:category].blank?
+      @job_posts = JobPost.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @job_posts = JobPost.where(category_id: @category_id).order("created_at DESC")
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@job = Job.new
-	end
+  def new
+    @job_post = JobPost.new
+  end
 
-	def create
-		@job = Job.new(jobs_params)
+  def create
+    @job_post = JobPost.new(job_posts_params)
 
-		if @job.save
-			redirect_to @job
-		else
-			render "New"
-		end
-	end
+    if @job_post.save
+      redirect_to @job_post
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @job.update(jobs_params)
-			redirect_to @job
-		else
-			render "Edit"
-		end
-	end
+  def update
+    if @job_post.update(job_posts_params)
+      redirect_to @job_post
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@job.destroy
-		redirect_to root_path
-	end
+  def destroy
+    @job_post.destroy
+    redirect_to root_path
+  end
 
-	private
+  private
 
-	def jobs_params
-		params.require(:job).permit(:title, :description, :company, :url, :category_id)
-	end
+  def job_posts_params
+    params.require(:job_post).permit(:title, :description, :company, :url, :category_id)
+  end
 
-	def find_job
-		@job = Job.find(params[:id])
-	end
+  def find_job_post
+    @job_post = JobPost.find(params[:id])
+  end
 end
